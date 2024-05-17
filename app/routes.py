07c0b8e5d4d6f403cd265208e06db60e01eb31d7
VITOR CAVALLINI DE SOUZA <vitor.csouza14@senacsp.edu.br>
2024-05-17 20:36:23 -0300

    Sexto Commit

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,26 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html', titulo="Página Inicial", nome="Vitor Cavallini")
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html', titulo="Sobre", nome="Vitor Cavallini")
+
+@app.route('/login')
+def login():
+    return render_template('login.html', titulo="Login", nome="Vitor Cavallini")
+@app.route('/cadastrar')
+def cadastrar():
+    return render_template('cadastrar.html', titulo="Cadastro", nome="Vitor Cavallini")
+
+@app.route('/palmeiras')
+def palmeiras():
+    return render_template('palmeiras.html', titulo="Palmeiras", nome="Vitor Cavallini")
+
+@app.route('/real')
+def real():
+    return render_template('real.html', titulo="Real Madrid", nome="Vitor Cavallini")
+
+@app.route('/vasco')
+def vasco():
+    return render_template('vasco.html', titulo="Vasco da Gama", nome="Vitor Cavallini")
